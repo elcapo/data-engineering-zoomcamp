19e97ff4c47e6104ad807590e0a37e622abade80
Carlos Capote <carlos.capote@hawara.es>
2026-03-24 18:35:08 +0000

    update the log queries so that they point to the boc_log schema

--- proyecto-analisis-del-boc/pipeline/sql/metrics/issue-details.sql
+++ proyecto-analisis-del-boc/pipeline/sql/metrics/issue-details.sql
@@ -13,6 +13,6 @@ SELECT
   el.extracted_at
 FROM boc_dataset.issue__dispositions AS d
 JOIN boc_dataset.issue AS i ON i._dlt_id = d._dlt_parent_id
-LEFT JOIN boc_dataset.download_log AS dl ON dl.entity_type = 'document' AND dl.year = i.year AND dl.issue = i.issue AND dl.disposition = (d._dlt_list_idx + 1)
-LEFT JOIN boc_dataset.extraction_log AS el ON el.entity_type = 'document'AND el.year = i.year AND el.issue = i.issue AND el.disposition = (d._dlt_list_idx + 1)
+LEFT JOIN boc_log.download_log AS dl ON dl.entity_type = 'document' AND dl.year = i.year AND dl.issue = i.issue AND dl.disposition = (d._dlt_list_idx + 1)
+LEFT JOIN boc_log.extraction_log AS el ON el.entity_type = 'document'AND el.year = i.year AND el.issue = i.issue AND el.disposition = (d._dlt_list_idx + 1)
 ORDER BY i.year, i.issue, d._dlt_list_idx
--- proyecto-analisis-del-boc/pipeline/sql/metrics/year-details.sql
+++ proyecto-analisis-del-boc/pipeline/sql/metrics/year-details.sql
@@ -10,6 +10,6 @@ SELECT
   dl.downloaded_at,
   el.extracted_at
 FROM boc_dataset.year AS y
-LEFT JOIN boc_dataset.download_log AS dl ON dl.entity_type = 'issue' AND dl.year = y.year::INT AND dl.issue = y.issue
-LEFT JOIN boc_dataset.extraction_log AS el ON el.entity_type = 'issue'AND el.year = y.year::INT AND el.issue = y.issue
+LEFT JOIN boc_log.download_log AS dl ON dl.entity_type = 'issue' AND dl.year = y.year::INT AND dl.issue = y.issue
+LEFT JOIN boc_log.extraction_log AS el ON el.entity_type = 'issue'AND el.year = y.year::INT AND el.issue = y.issue
 ORDER BY y.year::INT DESC, y.issue::INT DESC
